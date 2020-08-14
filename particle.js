928ec7f64d7ed4ae1dbd74bf3c6dfd9d221d90b7
shamita1512 <chemica1cooki3@gmail.com>
2020-08-14 16:32:45 +0530

    particle

---
+++ particle.js
@@ -0,0 +1,25 @@
+class Particle{
+    constructor(x,y,r){
+        var options ={
+            'restitution':0.4,
+            'density': 0.2,
+            
+        }
+        
+        this.body = Bodies.circle(x, y, r, options);
+        this.r = r;
+        this.color =color(random(0,255),random(0,255),random(0,255));
+        World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        noStroke();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r, this.r);
+        pop();
+      }
+    }
